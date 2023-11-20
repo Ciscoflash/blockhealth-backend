96b69015255db87b3acd1d6b3d794bfe566c264e
Ciscoflash <abrahamjude1999@gmail.com>
2023-11-20 10:33:27 +0100

    index conflict resolved

--- index.js
+++ index.js
@@ -2,38 +2,39 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose")
-const hospitalRouter = require('./routes/Hospital')
-const authRouter = require("./routes/Auth")
-const patientRouter = require("./routes/Patient")
-const medicationRouter = require("./routes/Medication")
-const allergyRouter = require("./routes/Allergies")
+const mongoose = require("mongoose");
+const hospitalRouter = require("./routes/Hospital");
+const authRouter = require("./routes/Auth");
+const patientRouter = require("./routes/Patient");
+const medicationRouter = require("./routes/Medication");
+const allergyRouter = require("./routes/Allergies");
 
 const app = express();
-const baseUrl = process.env.BASE_API
+const baseUrl = process.env.BASE_API;
 
 // Mount Middlewares
-app.use(cors())
+app.use(cors());
 app.options("*", cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-app.use(`${baseUrl}/hospitals`, hospitalRouter)
-app.use(`${baseUrl}/auth`, authRouter)
-app.use(`${baseUrl}/hospitals`, patientRouter)
-app.use(`${baseUrl}/hospitals`, medicationRouter)
-app.use(`${baseUrl}/hospitals`, allergyRouter)
+app.use(`${baseUrl}/hospitals`, hospitalRouter);
+app.use(`${baseUrl}/auth`, authRouter);
+app.use(`${baseUrl}/hospitals`, patientRouter);
+app.use(`${baseUrl}/hospitals`, medicationRouter);
+app.use(`${baseUrl}/hospitals`, allergyRouter);
 
 // DB Connect
-mongoose.connect(process.env.DB_URL)
-    .then(()=>console.log("Database Connected Successfully"))
-    .catch((error) => console.error(error))
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((error) => console.error(error));
 
 app.get(baseUrl, (req, res) => {
-    res.send("Hello, Welcome")
-})
+  res.send("Hello, Welcome");
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on ${process.env.PORT}`)
-})
+  console.log(`Server is listening on ${process.env.PORT}`);
+});
